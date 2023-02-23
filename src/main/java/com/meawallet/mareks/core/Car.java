0a5d21023650823eb31823d23fb7e74582e2990a
Mareks <robaldsmareks@gmail.com>
2023-02-23 10:35:54 +0200

    Added car class

--- src/main/java/com/meawallet/mareks/core/Car.java
+++ src/main/java/com/meawallet/mareks/core/Car.java
@@ -1,4 +1,22 @@
 package com.meawallet.mareks.core;
 
 public class Car {
+
+    String Model;
+    String Brand;
+    Integer Year;
+
+    enum engineType{
+        Petrol,
+        Diesel,
+        Electric,
+        Hybrid
+    }
+
+    Double engineVolume;
+    Short horsePower;
+    Integer drivenKm;
+    String numberPlate;
+
 }
+
